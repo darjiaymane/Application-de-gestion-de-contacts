32b4b3a18d741da6c3da6f997b25eb1518159c49
aymane <aymanedarj7@gmail.com>
2022-04-27 09:52:16 +0000

    User class/signUp method Add the SignUp method to the class user

--- user.php
+++ user.php
@@ -11,7 +11,33 @@ class User{
     {
         $this->conx = $conn;
     }
-    
+    function signup( $fname, $lname, $username, $password){
+
+            $date = date('d-m-Y h:i:sa');
+            $_SESSION['dateUp']=$date;
+            $_SESSION['dateIN']=$date;
+
+            $sql_insert = "INSERT INTO user (`fname`,`lname`,`username`, `password`, `date`) VALUES (?,?,?,?,?)";
+
+            try {
+                $REQ= $this->conx->prepare($sql_insert);
+                $REQ->execute([$fname, $lname, $username , $password, $date]);
+                $_SESSION['Fname'] = $fname;
+                $_SESSION['Lname'] = $lname;
+                $req= $this->conx->prepare("SELECT id FROM user WHERE username=?");
+                $req->execute([$username]);
+                $req=$req->fetch(PDO::FETCH_ASSOC);
+                $_SESSION['id']=$req['id'];
+                header("location:profil.php");
+
+            }catch (Exception $e){
+                $_SESSION['error4']="The username is already exist";
+                header("location:signup.php");
+            }
+
+
+    }
+
 
 
 }
