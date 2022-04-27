17a505f2d1155d519fd2fb0879764a3c1f421825
aymane <aymanedarj7@gmail.com>
2022-04-27 09:53:46 +0000

    User class/ SignIn method Add the signIn method to the class user

--- user.php
+++ user.php
@@ -38,6 +38,43 @@ class User{
 
     }
 
+/***************************Login*****************************/
+    function login( $username, $password ){
+
+        if (empty($username)) {
+            $_SESSION['error1'] = "please enter your email";
+            header('location:signin.php');
+        }
+        if (empty($password)) {
+            $_SESSION['error2'] = "please enter your password";
+            header('location:signin.php');
+        }
+        if (!empty($username) && !empty($password)) {
+
+            $sql_valid = "SELECT * FROM user WHERE username=? AND password=? ";
+            $REQ= $this->conx->prepare($sql_valid);
+            $REQ->execute([$username , $password]);
+            $REQ=$REQ->fetch(PDO::FETCH_ASSOC);
+            if ($REQ) {
+
+                $_SESSION['Fname'] = $REQ['fname'];
+                $_SESSION['Lname'] = $REQ['lname'];
+                $_SESSION['id']=$REQ['id'];
+                $_SESSION['dateUp']=$REQ['date'];
+                date_default_timezone_set('Africa/Casablanca');
+                $date = date('d-m-Y h:i:sa');
+                $_SESSION['dateIN']=$date;
+
+               header('location:profil.php');
+
+            } else {
+                $_SESSION['wrong_info']="Username or Password is incorrect";
+                header('location:signin.php');
+            }
+        }
+    }
+
+    
 
 
 }
